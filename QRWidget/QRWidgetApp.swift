225401fca3c1aeb173aadcbd2c7c2a593b99a994
Vitaly <v.yamschikov@blanc.ru>
2021-11-15 01:00:38 +0500

    Initial Commit

---
+++ QRWidget/QRWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRWidgetApp.swift
+//  QRWidget
+//
+//  Created by Vitaliy on 15.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct QRWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
